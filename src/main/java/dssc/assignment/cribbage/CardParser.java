d3ca1454cc70b8088f52038728560b09a9f73c4c
Domenis Nicola S262688 <nicoladomenis23@hotmail.it>
2020-12-08 12:59:00 +0100

    Corrected by issue "Unnecessary complex condition"

--- src/main/java/dssc/assignment/cribbage/CardParser.java
+++ src/main/java/dssc/assignment/cribbage/CardParser.java
@@ -3,7 +3,7 @@ package dssc.assignment.cribbage;
 public class CardParser {
     public static Card parseCard(String cardAsText) {
         for (Suite refSuite : Suite.values()){
-            if(Character.compare(refSuite.toString().charAt(0),cardAsText.charAt(1))==0)
+            if(refSuite.asChar() == cardAsText.charAt(1))
                 return new Card(cardAsText.charAt(0),refSuite);
         }
         return new Card(cardAsText.charAt(0),Suite.INVALID);
