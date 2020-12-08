fad9b7688ddd84c2cd9fad7dd0675d1c538739d5
Domenis Nicola S262688 <nicoladomenis23@hotmail.it>
2020-12-08 13:02:53 +0100

    Renamed methods of class PointsCalculator

--- src/main/java/dssc/assignment/cribbage/PointCalculator.java
+++ src/main/java/dssc/assignment/cribbage/PointCalculator.java
@@ -3,13 +3,12 @@ import java.util.Arrays;
 public class PointCalculator {
 
 
-    public int calculatePoints(Hand hand) {
-        int points = fifteen_two_points(hand) + pairs_points(hand) + jack_points(hand) + flush_points(hand)+runs_points(hand);
-        return points;
+    public int calculateTotalPoints(Hand hand) {
+        return calculate_fifteen_two_points(hand) + calculate_pairs_points(hand) + calculate_jack_points(hand) + calculate_flush_points(hand)+calculate_runs_points(hand);
     }
 
 
-    public int fifteen_two_points(Hand hand) {
+    public int calculate_fifteen_two_points(Hand hand) {
         FifteenTwoCalculator fifteenTwoCalculator = new FifteenTwoCalculator();
         int score = 0;
         int sum = 0;
@@ -23,7 +22,7 @@ public class PointCalculator {
         score += fifteenTwoCalculator.countAllCardsForFiftheenTwo(hand);
         return score/2;
     }
-    public int pairs_points(Hand hand) {
+    public int calculate_pairs_points(Hand hand) {
         PairsCalculator pairsCalculator = new PairsCalculator();
         int addPoints = 0;
         for (int i = 1; i < 6; i++) {
@@ -33,16 +32,16 @@ public class PointCalculator {
         }
         return addPoints ;
     }
-    public int jack_points(Hand hand) {
+    public int calculate_jack_points(Hand hand) {
         JackCalculator jackCalc = new JackCalculator();
         return jackCalc.jackCalculator(hand);
     }
-    public int flush_points(Hand hand) {
+    public int calculate_flush_points(Hand hand) {
 
         FlushCalculator flushCalc= new FlushCalculator();
         return flushCalc.flushCalculator(hand);
     }
-    public int runs_points(Hand hand){
+    public int calculate_runs_points(Hand hand){
         RunsCalculator runsCalc =new RunsCalculator();
         int[] flags = runsCalc.find_cards_composition(hand);
         return runsCalc.solveRunsPoints(flags);
